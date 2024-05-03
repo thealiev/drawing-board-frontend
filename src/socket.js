dc475c9092bfb2a802726831d901760e5f713683
Salohiddin Tojiyev <129267512+thealiev@users.noreply.github.com>
2024-05-03 15:08:51 +0500

    done

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,6 @@
 import { io } from "socket.io-client";
-const URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000'
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://drawing-board-frontend-alpha.vercel.app/"
+    : "http://localhost:5000";
 export const socket = io(URL);
